2c343078e624787be47c40d0ceff2cab3baee36e
Th1nkK1D <witheep@gmail.com>
2022-04-13 14:56:00 +0300

    Add title, description and favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,12 @@
 	import Hero from '../components/landing/hero.svelte';
 </script>
 
+<svelte:head>
+	<title>Burmese-Thai Film Screening</title>
+	<meta
+		name="description"
+		content="A films screening of Thai and Burmese political films under military government"
+	/>
+</svelte:head>
+
 <Hero />
